6a4ff4103f9a8ba088e1af8f934020cee34008b8
calebeggers <calebeggers11@gmail.com>
2016-07-29 13:57:41 -0400

    Added a resume mockup and resume form.

--- src/js/utilities/config.js
+++ src/js/utilities/config.js
@@ -1,66 +1,54 @@
 function config ($stateProvider, $urlRouterProvider){
 
-    $stateProvider
-        .state('root', {
-            abstract: true,
-            templateUrl: 'templates/layout.temp.html'
-        })
-        .state('root.home', {
-        	url: '/',
-        	templateUrl: 'templates/home.temp.html',
-        	controller: 'HomeController as vm'
-        })
-        .state('root.portfolios', {
-            url: '/portfolios',
-            templateUrl: 'templates/portfolios.temp.html'
-        })
-        .state('root.login', {
-            url: '/login',
-            templateUrl: 'templates/login.temp.html',
-            controller: 'LoginController as vm'
-        })
-        .state('root.register', {
-            url: '/register',
-            templateUrl: 'templates/register.temp.html',
-            controller: 'RegisterController as vm'
-        })
-        .state('root.profile-info', {
-            url: '/edit-profile-info',
-            templateUrl: 'templates/profile-info.temp.html',
-            controller: 'ProfileFormController as vm'
-        })
-        .state('root.profile', {
+	$stateProvider
+		.state('root', {
+			abstract: true,
+			templateUrl: 'templates/layout.temp.html'
+		})
+		.state('root.home', {
+			url: '/',
+			templateUrl: 'templates/home.temp.html',
+			controller: 'HomeController as vm'
+		})
+		.state('root.portfolios', {
+			url: '/portfolios',
+			templateUrl: 'templates/portfolios.temp.html'
+		})
+		.state('root.login', {
+			url: '/login',
+			templateUrl: 'templates/login.temp.html',
+			controller: 'LoginController as vm'
+		})
+		.state('root.register', {
+			url: '/register',
+			templateUrl: 'templates/register.temp.html',
+			controller: 'RegisterController as vm'
+		})
+		.state('root.profile-info', {
+			url: '/edit-profile-info',
+			templateUrl: 'templates/profile-info.temp.html',
+			controller: 'ProfileFormController as vm'
+		})
+		.state('root.profile', {
+			url: '/:username',
+			templateUrl: 'templates/profile.temp.html',
+			controller: 'ProfileController as vm'
 
-            url: '/:username',
-            templateUrl: 'templates/profile.temp.html',
-            controller: 'ProfileController as vm'
+		})
+		.state('root.error', {
+			url: '/404',
+			templateUrl: 'templates/error.temp.html'
+		})
+		.state('resume', {
+			url: '/:username/resume',
+			templateUrl: 'templates/resume.temp.html'
+		})
+		.state('edit', {
+			url: '/:username/resume/edit',
+			templateUrl: 'templates/edit.temp.html'
+		})
 
-        })
-        // .state('root.resume', {
-        //     url: '/:username/resume',
-        //     templateUrl: 'templates/resume.temp.html'
-        // })
-        .state('root.edit', {
-            url: '/:username/resume/edit',
-            templateUrl: 'templates/edit.temp.html'
-        })
-        .state('root.error', {
-            url: '/404',
-            templateUrl: 'templates/error.temp.html'
-        })
-        // .state('root2', {
-        //     abstract: true,
-        //     templateUrl: 'template/resume-layout.temp.html'
-        // })
-        .state('resume', {
-            url: '/:username/resume',
-            templateUrl: 'templates/resume.temp.html'
-        })
-
-        $urlRouterProvider.otherwise('/404');
-       
-
-
+		$urlRouterProvider.otherwise('/404');
 }
 
 config.$inject = ['$stateProvider', '$urlRouterProvider'];
